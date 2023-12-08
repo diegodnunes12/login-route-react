b92fda093095817a432d254cdaf3834b504c5edf
Diego Nunes <diegodnunes12@gmail.com>
2023-12-08 18:09:37 -0300

    criando o projeto

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import { Button } from "../../components/Button";
+
+const Home = () => {
+    return (
+        <>
+            <Button title="Teste1" />
+            <Button title="Teste2" variant="secondary" />
+        </>
+    )
+}
+
+export { Home };
