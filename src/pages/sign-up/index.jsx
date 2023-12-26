55aa4c78acc26ebcfba11062493cd7ae73e61331
Diego Nunes <diegodnunes12@gmail.com>
2023-12-26 18:08:17 -0300

    criando a nova conta

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -1,7 +1,72 @@
+import { useForm } from 'react-hook-form';
+import { useNavigate } from "react-router-dom";
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { A, H1, H2, Main, P, Form, Section } from './styles';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { AccountBoxSVGIcon, EmailSVGIcon, LockSVGIcon } from '@react-md/material-icons';
+import { Button } from '../../components/Button';
+import { api } from '../../services/api';
+
+const schema = yup
+  .object({
+    fullName: yup.string().min(5, "Min caracter is 5").required("This field is required"),
+    email: yup.string().min(10, "Min caracter is 10").email("Email invalid").required("This field is required"),
+    password: yup.string().min(6, "Min caracter is 6").required("This field is required"),
+  })
+  .required();
+
 
 const Signup = () => {
+  const navigate = useNavigate();
+
+  const {handleSubmit, control, formState: { errors, isValid }} = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+    }
+  });
+  
+  const handleSubmitForm = async (form) => {
+    try {
+      if(isValid) {
+        const { data } = await api.post(`/users`, form);
+
+        if(data?.id) {
+          navigate("/feed");
+        }
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
-    <div>index</div>
+    <>
+      <Header />
+      <Main>
+        <Section>
+          <H1>
+            The platform for you to learn from experts, master the main technologies and enter the most desired companies faster.
+          </H1>
+        </Section>
+        <Section>
+          <H2>Start now free</H2>
+          <P>Create your account and make the change.</P>
+          <Form onSubmit={handleSubmit(handleSubmitForm)}>
+            <Input type="text" name="fullName" placeholder="Full Name" control={control} leftIcon={<AccountBoxSVGIcon width="20px" errorMessage={errors?.fullName?.message} />} />
+            <Input type="email" name="email" placeholder="E-mail" control={control} leftIcon={<EmailSVGIcon width="20px" />} errorMessage={errors?.email?.message} />
+            <Input type="password" name="password" placeholder="Password" control={control} leftIcon={<LockSVGIcon width="20px" errorMessage={errors?.password?.message} />} />
+            <Button title="Create my free account" type="submit" />
+          </Form>
+          <P>already have an account <A href="/login">Log in</A>.</P>
+        </Section>
+      </Main>
+    </>
   )
 }
 
---
+++ src/pages/sign-up/styles.js
@@ -0,0 +1,50 @@
+import { styled } from 'styled-components';
+
+export const Main = styled.main`
+    width: 80%;
+    margin: 100px auto;
+    display: flex;
+`
+
+export const Section = styled.section`
+    width: 50%;
+`
+
+export const Form = styled.form`
+    width: 100%;
+    margin-bottom: 40px;
+`
+
+export const H1 = styled.h1`
+    color: #fff;
+    font-family: 'Open Sans';
+    font-size: 32px;
+    width: 300px;
+    margin: auto;
+`
+
+export const H2 = styled.h2`
+    color: #fff;
+    font-family: 'Open Sans';
+    font-size: 28px;
+    margin-bottom: 20px;
+`
+
+export const P = styled.p`
+    color: #fff;
+    font-family: 'Open Sans';
+    font-size: 16px;
+    margin-bottom: 30px;
+    margin-top: 20px;
+`
+
+export const A = styled.a`
+    color: rgb(35, 221, 122);
+    font-family: 'Open Sans';
+    font-size: 16px;
+
+    &:hover {
+        cursor: pointer;
+    }
+`
+
